3bc57615537432e629b642bced6361e550cd9d9d
shrey9393 <shreyanshagrawal9393@gmail.com>
2023-05-04 00:56:39 +0530

    changes

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 export default function Navbar(props) {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
         <a className="navbar-brand" href="/">
           {props.title}
@@ -49,7 +49,5 @@ export default function Navbar(props) {
   );
 }
 
-Navbar.prototype = { title: PropTypes.string, 
-                    aboutText: PropTypes.string };
-Navbar.defaultProps= { title: "Add title", 
-                    aboutText: PropTypes.string };
+Navbar.prototype = { title: PropTypes.string, aboutText: PropTypes.string };
+Navbar.defaultProps = { title: "Add title", aboutText: PropTypes.string };
--- src/Components/TextFrom.js
+++ src/Components/TextFrom.js
@@ -1,31 +1,54 @@
 import React, { useState } from "react";
 
 export default function TextFrom(props) {
-  const [text, setText] = useState("ENTER TEXT HERE");
+  const [text, setText] = useState("");
 
-  const handleUpClick = () => {
+  const toUpperCase = () => {
     let newText = text.toUpperCase();
+    setText(newText);
+  };
+
+  const toLowerCase = () => {
+    let newText = text.toLowerCase();
     setText(newText);
   };
 
   const handelOnChange = (event) => {
     setText(event.target.value);
   };
+
   return (
-    <div>
-      <h1>{props.heading}</h1>
-      <div className="mb-3">
-        <textarea
-          className="form-control"
-          value={text}
-          onChange={handelOnChange}
-          id="myBox"
-          rows="8"
-        ></textarea>
+    <>
+      <div>
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handelOnChange}
+            id="myBox"
+            rows="8"
+          ></textarea>
+        </div>
+        <button type="button" onClick={toUpperCase} className="btn btn-primary">
+          Convert to upper case
+        </button>
+        <button
+          type="button"
+          onClick={toLowerCase}
+          className="btn btn-primary mx-3"
+        >
+          Convert to lower case
+        </button>
       </div>
-      <button type="button" onClick={handleUpClick} className="btn btn-primary">
-        Convert to upper case
-      </button>
-    </div>
+      <div className="container my-3">
+        <h1>your text summery</h1>
+        <p>
+          {text.split(" ").length} words, {text.length} character
+        </p>
+        <h1>Preview</h1>
+        <p>{text}</p>
+      </div>
+    </>
   );
 }
